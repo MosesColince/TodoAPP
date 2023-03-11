539495c51acbc74169363ad23b7a32e2ca27a1c0
Sithomola Mushe <mosescolince161@gmail.com>
2023-03-11 03:26:34 +0200

    create a sign in page#2

--- todoapp/lib/loginscreen/login.dart
+++ todoapp/lib/loginscreen/login.dart
@@ -156,7 +156,7 @@ class _LoginScreenState extends State<LoginScreen> {
       child: RichText(
           text: TextSpan(children: [
         TextSpan(
-            text: "Don't habe an account?",
+            text: "Don't have an account?",
             style: TextStyle(
                 fontSize: 17,
                 color: Colors.black,
@@ -175,51 +175,50 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AnnotatedRegion<SystemUiOverlayStyle>(
-        value: SystemUiOverlayStyle.light,
-        child: GestureDetector(
-          child: Stack(
-            children: <Widget>[
-              Container(
-                height: double.infinity,
-                width: double.infinity,
-                decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                      Color.fromARGB(255, 215, 224, 43),
-                      Color.fromARGB(255, 215, 224, 43),
-                      Color.fromARGB(255, 215, 224, 43),
-                      Color.fromARGB(255, 215, 224, 43),
-                    ])),
-                child: SingleChildScrollView(
-                  physics: AlwaysScrollableScrollPhysics(),
-                  padding: EdgeInsets.symmetric(horizontal: 25, vertical: 120),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      // ignore: prefer_const_constructors
-                      Text(
-                        "Sign in",
-                        style: const TextStyle(
-                            color: Color.fromARGB(255, 37, 140, 59),
-                            fontSize: 40,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(height: 50),
-                      buildEmail(),
-                      SizedBox(height: 20),
-                      buildPassword(),
-                      buildForgotPassWordButton(),
-                      buildRememberbutton(),
-                      buildLoginButton(),
-                      buildSignUpButton(),
-                    ],
+      body: SingleChildScrollView(
+        child: AnnotatedRegion<SystemUiOverlayStyle>(
+          value: SystemUiOverlayStyle.light,
+          child: GestureDetector(
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  height: double.infinity,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                        Color.fromARGB(255, 215, 224, 43),
+                        Color.fromARGB(255, 215, 224, 43),
+                        Color.fromARGB(255, 215, 224, 43),
+                        Color.fromARGB(255, 215, 224, 43),
+                      ])),
+                  child: SingleChildScrollView(
+                    child: Stack(
+                      children: <Widget>[
+                        // ignore: prefer_const_constructors
+                        Text(
+                          "Sign in",
+                          style: const TextStyle(
+                              color: Color.fromARGB(255, 37, 140, 59),
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 50),
+                        buildEmail(),
+                        SizedBox(height: 20),
+                        buildPassword(),
+                        buildForgotPassWordButton(),
+                        buildRememberbutton(),
+                        buildLoginButton(),
+                        buildSignUpButton(),
+                      ],
+                    ),
                   ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
